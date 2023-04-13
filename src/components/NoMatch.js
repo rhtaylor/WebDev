97584c5f194c2836753b3468deb1a9a218aa9a75
williehydro <grow.hydros@gmail.com>
2023-04-13 13:07:56 -0700

    update

---
+++ src/components/NoMatch.js
@@ -0,0 +1,12 @@
+import '../scss/software-dev.scss'
+
+const NoMatch = () =>{
+
+    return(<>
+                <div className="d-flex nomatch justify-content-center">
+                    <p className="my-auto">Page Not found: 404!</p>
+                </div>
+           </>)
+}
+
+export default NoMatch;
