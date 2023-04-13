73cb958ed69c1d24db5444085cd1cf43d8c1daf7
williehydro <grow.hydros@gmail.com>
2023-04-13 15:09:32 -0700

    updates position && nav bar style

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom"
 const Navigation = () =>{
 
     return(<nav className="d-flex justify-content-center">
-           <Link className="px-4" to="/home">Home</Link>
-           <Link className="px-4" to="/resume">Resume</Link>
-           <Link className="px-4" to="/technologies">Tech</Link>
+           <Link className="h1 px-4 text-danger bg-dark" to="/home">Home</Link>
+           <Link className="h1 px-4 text-danger bg-dark" to="/resume">Resume</Link>
+           <Link className="h1 px-4 text-danger bg-dark" to="/technologies">Tech</Link>
           </nav>
     )
 }
